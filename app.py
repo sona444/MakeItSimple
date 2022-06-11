cc18c8ee4edc9538cd93c2902de5c81bb3777221
Sonakshi <sonakshi2500@gmail.com>
2022-06-11 21:22:19 +0530

    changes for basic login

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 from flask import Flask, jsonify, render_template, request
 from dotenv import load_dotenv
-from flask_login import LoginManager,login_user, login_required
-# import json
-# from models import User
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-import os
 
-login_manager = LoginManager()
-login_manager.login_view = 'login'
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://sonakshi:sonakshi@localhost/gems"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 load_dotenv()
-login_manager.init_app(app)
+
+from models import User, Role, UserRoles
 
 @app.route("/")
 def index():
@@ -23,20 +19,8 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
-    # info = json.loads(request.data)
-    # username = info.get('username', 'guest')
-    # password = info.get('password', '') 
-    # user = User.objects(name=username,
-    #                     password=password).first()
-    # if user:
-    #     login_user(user)
-    #     return jsonify(user.to_json())
-    # else:
-    #     return jsonify({"status": 401,
-    #                     "reason": "Username or Password Error"})
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.objects(id=user_id).first()
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -6,10 +6,13 @@ class User(db.Model):
     name=db.Column(db.String,nullable=False)
     password_hash = db.Column(db.String(length=100), nullable=True)
     email = db.Column(db.String(50), nullable=True) 
-    roles = db.relationship('Role', secondary='user_roles')                                                                                              
-    def to_json(self):        
-        return {"name": self.name,
-                "email": self.email}
+    roles = db.relationship('Role', secondary='user_roles')   
+                                                                                               
+    def __init__(self, name, password_hash,email, roles):        
+        self.name=name
+        self.password_hash=password_hash
+        self.email=email
+        self.roles=roles
 
 
 class Role(db.Model):
@@ -17,9 +20,16 @@ class Role(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), unique=True)
 
+    def __init__(self, name):        
+        self.name=name
+
 # Define the UserRoles association table
 class UserRoles(db.Model):
     __tablename__ = 'user_roles'
     id = db.Column(db.Integer(), primary_key=True)
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+    def __init__(self, user_id, role_id):        
+        self.user_id=user_id
+        self.role_id=role_id
