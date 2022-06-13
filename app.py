ab1b8db5a360b7a6990ff0bb951311b651550a15
Sonakshi <sonakshi2500@gmail.com>
2022-06-14 01:09:50 +0530

    course add and tagging done

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 load_dotenv()
 
-from models import User, Role, UserRoles
+from models import *
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    return render_template("login.html")
+# @app.route("/login", methods=['GET', 'POST'])
+# def login():
+#     return render_template("login.html")
     
-@app.route("/admin", methods=['GET', 'POST'])
+@app.route("/add-users", methods=['GET', 'POST'])
 def admin():
     roles=Role.query.all()
     return render_template("admin.html", roles=roles)
@@ -78,6 +78,65 @@ def dashboard(hash):
     user=User.query.filter(User.email==hash).first()
 
     return render_template("dashboard.html",user=user)
+
+@app.route("/add-new-course", methods=['GET', 'POST'])
+def coursesAdd():
+    
+        cname=request.form.get('course-name')
+        credit=request.form.get('credits')
+        type=request.form.get('type')
+        if cname and credit and type:
+                    db.session.add(Courses(name=cname,  credit=credit, course_type=type))
+                    db.session.commit()
+        else:
+            return("Please fill all the fields")
+        return ("New course has been added successfully!")
+
+@app.route("/new-course", methods=['GET', 'POST'])
+def courses():
+    return render_template('add-courses.html')
+
+@app.route("/register-courses", methods=['GET', 'POST'])
+def registerCourses():
+    users=User.query.all()
+    courses=Courses.query.all()
+    return render_template('course-register.html', users=users, courses=courses)
+
+@app.route("/get-user-roles",methods=['GET','POST'])
+def getRoles():
+    role=request.form.get('role')
+    username=request.form.get('user')
+    user=User.query.filter(User.name==username).first()
+    usercourses=UserCourses.query.filter(UserCourses.user_id==user.id).all()
+    courses=[]
+    for i in usercourses:
+        course=Courses.query.filter(Courses.id==i.course_id).first()
+        courses.append(course.name)
+    print(courses)
+    return {"courses":courses}
+
+@app.route("/tag-course", methods=['GET', 'POST'])
+def coursestag():
+    
+        role=request.form.get('role')
+        faculty=request.form.get('faculty')
+        student=request.form.get('student')
+        course=request.form.get('course')
+        if role and (faculty or student) and course:
+            if faculty:
+                userId=User.query.filter(User.name==faculty).first()
+            elif student:
+                userId=User.query.filter(User.name==student).first()
+            Usercourse=UserCourses.query.filter(UserCourses.user_id==userId.id).all()
+            courseId=Courses.query.filter(Courses.name==course).first()
+            for i in Usercourse:
+                if i.course_id==courseId.id:
+                    return("User is already tagged with this course!")
+            db.session.add(UserCourses(user_id=userId.id, course_id=courseId.id))
+            db.session.commit()
+        else:
+            return("Please fill all the fields")
+        return ("Course has been tagged to the specified user!")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=True) 
     phone=db.Column(db.String(50),nullable=False)
     roles=db.Column(db.String(50),nullable=False)
+    roll_no=db.Column(db.String(50),nullable=True)
 
     def __init__(self, name, password_hash,email, roles, phone):        
         self.name=name
@@ -35,3 +36,55 @@ class UserRoles(db.Model):
     def __init__(self, user_id, role_id):        
         self.user_id=user_id
         self.role_id=role_id
+
+class Attendance(db.Model):
+    __tablename__ = 'attendance'
+    id=db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    student_name = db.Column(db.String,nullable=False)
+    date =db.Column(db.DateTime)
+    course_id=db.Column(db.Integer(), db.ForeignKey('courses.id', ondelete='CASCADE'))
+    course_name=db.Column(db.String(20),nullable=False)
+
+    def __init__(self, user_id, student_name, date, course_id, course_name):        
+        self.user_id=user_id
+        self.student_name=student_name
+        self.date=date
+        self.course_id=course_id
+        self.course_name=course_name
+
+class Courses(db.Model):
+    __tablename__ = 'courses'
+    id=db.Column(db.Integer(), primary_key=True)
+    name=db.Column(db.String,nullable=False)
+    credit=db.Column(db.Integer,nullable=False)
+    course_type=db.Column(db.String,nullable=False)
+    
+    def __init__(self, name, credit, course_type):
+        self.name=name
+        self.credit=credit
+        self.course_type=course_type
+
+class Timetable(db.Model):
+    __tablename__ = 'timetable'
+    id=db.Column(db.Integer(), primary_key=True)
+    day=db.Column(db.String(20),nullable=False)
+    time=db.Column(db.Time, nullable=False)
+    course_id=db.Column(db.Integer(), db.ForeignKey('courses.id', ondelete='CASCADE'))
+    course_name=db.Column(db.String(20),nullable=False)
+
+    def __init__(self, day, time, course_id, course_name):
+        self.day=day
+        self.time=time
+        self.course_id=course_id
+        self.course_name=course_name
+
+class UserCourses(db.Model):
+    __tablename__='user-courses'
+    id=db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    course_id = db.Column(db.Integer(), db.ForeignKey('courses.id', ondelete='CASCADE'))
+
+    def __init__(self, user_id, course_id):        
+        self.user_id=user_id
+        self.course_id=course_id
